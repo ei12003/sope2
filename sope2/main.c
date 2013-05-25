df8690f82a2aad701a42eb55278d6445153ac562
ei12003 <ei12003@github.com>
2013-05-25 18:08:32 +0100

    Time/Cards

--- sope2/main.c
+++ sope2/main.c
@@ -4,8 +4,101 @@
 int isDealer;
 int fd_write[10];
 int fd_read,ownNUMBER;
+char hand[150];
 char ownFIFO[40], ownNAME[40];
-int log_fd;
+
+int play_card(char *hand, char* card);
+int log_game(char *what, char *result,int pnumber,char *name);
+void fill_str(int spaces, char *str, char *str_to_fill);
+
+char *gettime(){
+ time_t rawtime;
+  struct tm * timeinfo;
+
+        static char result[100];
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+
+    sprintf(result, "%d-%.2d-%.2d %.2d:%.2d:%.2d \n",
+    1900 + timeinfo->tm_year,
+    timeinfo->tm_mon,
+    timeinfo->tm_mday,
+    timeinfo->tm_hour,
+    timeinfo->tm_min,
+    timeinfo->tm_sec
+    );
+    
+    return result;
+
+}
+void read_hand(char* hand,char handcards[6][4]){
+int len=strlen(hand);
+int i,j,s,k=0;
+char rank[3]={0};
+char suit;
+
+
+//Ac-5c-8c-Jc/Ah-2h/6d-10d-Kd/3s-5s-4s-7s
+//read_hand("Jc/2h/10d-Kd/3s-5s");
+for(i=0;i<len;i++){
+    if(hand[i]!='/' && hand[i]!='-'){
+        rank[0]=hand[i];
+    
+        if(hand[i+1]=='0'){
+            i++;
+            rank[1]=hand[i];
+        }
+        
+        i++;
+        suit=hand[i];
+        
+        
+        for(j=0;j<strlen(rank);j++){
+            handcards[k][j]=rank[j];
+        }
+        handcards[k][j]=suit;
+        
+        k+=1;
+        memset(rank, '\0',sizeof(rank));
+        }
+    }
+    for(i=0;i<6;i++)
+        printf("card[%d]:%s\n",i,handcards[i]);
+    //printf("\nstr:%c",handcards[0][strlen(handcards[0])-1]);
+}
+
+
+void format_hand(char handcards[6][4],int size,char *hand_str){
+char str;
+int i,j;
+
+    char suit[]={'c','d','h','s'};
+    int check;
+    int c;
+
+    
+
+    for(i=0;i<4;i++){
+        for(j=0;j<size;j++){
+            if(handcards[j][strlen(handcards[j])-1]==suit[i]){
+                
+                check=1;
+                strcat(hand_str,handcards[j]);
+                strcat(hand_str,"-");
+            }
+
+        }
+if(check){
+        hand_str[strlen(hand_str)-1]='\0';
+        strcat(hand_str,"/");
+        check=0;
+        }
+    }
+    hand_str[strlen(hand_str)-1]='\0';
+    
+printf("\nstr:%s",hand_str);
+
+}
 
 int main(int argc, char *argv[], char *envp[])
 {
@@ -14,10 +107,16 @@ int main(int argc, char *argv[], char *envp[])
 shdata *addr;
 pthread_t tidp_gp,tidp_kbd,tidd;
 int shmid=0;
+char hand_str[200]="";
+char handcards[6][4]={0};
+char **var1;
+char *var2;
+read_hand("Jc/2h/10d-Kd/3s-5s",handcards);
 
+format_hand(handcards,6,hand_str);
 
-
-
+ printf("\n\n###\n%s",hand_str);
+/*
  if(argc!=4){
         printf("Invalid number of arguments\n");
         return -1;
@@ -66,44 +165,124 @@ return 0;
         Functions
    ################## */
 
-void log(char *what,char str,int pnumber,char *name,shdata *data){
-  char buf[];
-  char time[]="time_to_implement	|",player,what,result;
-  
-  strcat(buf,time);
-  strcat(buf,"Player");
-  // numberstrcat(buf,
-  strcat(buf,"-");
-  strcat(buf,name);
-  strcat(buf,"	|");
-  
-  write(log_fd,buf,sizeof(log_fd));  
+    //char buf[]="when    |who    |what   |result";
+
+void fill_str(int space, char *str, char *str_to_fill){
+
+int i,j=0,len=strlen(str);
+    if(space==1)
+        j=1;
+
+    for(i=0;i<len;i++)
+    {
+
+        str_to_fill[j+i]=str[i];
+    }
 }
 
+int log_game(char *action, char *outcome,int pnumber,char *name){
+  char aux[100]="";
+  char number[3]={((char)((int) '0'+pnumber)),'-'};
+  char when[21]={[0 ... 19] = ' '};
+  char what[18]={[0 ... 17] = ' '};
+  char result[44]={[0 ... 43] = ' '};
+ char who[21]={[0 ... 19] = ' '};
+ 
 
+int log_fd=open("shname.log", O_RDWR | O_APPEND);
+
+
+
+ int ctr=0;
+//log_game("receive-cards","Ac-5c/2s",0,"Manuel", data[0].log_fd);
+    /*19_|_18_|_15_|_42*/
+  /* when|who|what|result */
+
+
+    /*WHENfind time */
+    fill_str(0, gettime(), when);
+
+    ctr+=write(log_fd,when,sizeof(when)-1);  
+    write(log_fd,"|",sizeof("|")-1); 
+    /*WHO*/
+    if(strcmp(action,"deal")==0)
+        strcat(aux,"Dealer-");
+    else{
+        strcat(aux,"Player");
+        strcat(aux,number);
+    }
+    
+    strcat(aux,name);
+    //printf("aux:%s",aux);
+    fill_str(1, aux, who);
+    ctr+=write(log_fd,who,sizeof(who)-1); 
+    ctr+=write(log_fd,"|",sizeof("|")-1); 
+
+    /*WHAT*/
+    fill_str(1, action, what);
+    ctr+=write(log_fd,what,sizeof(what)-1); 
+   ctr+= write(log_fd,"|",sizeof("|")-1); 
+    
+    /*RESULT*/
+    fill_str(1, outcome, result);
+    ctr+=write(log_fd,result,sizeof(result)-1); 
+    ctr+=write(log_fd,"\n",sizeof("\n")-1); 
+
+    close(log_fd);
+    if(ctr<0)
+        return errno;
+    return ctr;
+  
+}
+
+int play_card(char *hand, char *card){
+
+int loc=strstr(hand,card);
+int len=strlen(card);
+if(loc==NULL)
+    return -1;
+return 0;
+    
+
+
+
+
+
+
+
+}
 void *player_kbd_handler(void *arg){
         shdata *addr=(shdata*)arg;
         char ch[100];
-        char hand[150];
+        
         int rank;
         char suit='W';
+        char aux[3];
+        char card[4];
         int c;
         
             while(1){
            printf("promptPLAYER > ");
             scanf("%s",ch);
             
-            if(strcmp(ch,"show")==0){
-                read(fd_read,hand,sizeof(hand));
-                printf("[Cards: %s]\n",hand);
+            if(strcmp(ch,"hand")==0){
+                printf("\nhandbytes:%d|\n",log_game("hand", hand,ownNUMBER,ownNAME));
+                printf("[Hand: %s]\n",hand);
             }
-            else if(strcmp(ch,"turn")==0)
+            else if(strcmp(ch,"turn")==0){
                 printf("[Turn: %d]\n",addr[0].turn);
-            else if(strstr(ch,"play:")!=0)
+                aux[0]=((char)((int) '0'+addr[0].turn));
+                printf("\nturnbytes:%d|\n",log_game("turn",aux,ownNUMBER,ownNAME));
+            }
+           /* else if(strstr(ch,"play:")!=NULL)
                 if(addr[0].turn!=ownNUMBER)
                     printf("It's not your turn to play!");
                 else{
-                    c=sscanf(ch,"play:%d%c",&rank,&suit);
+                    c=sscanf(ch,"play:%s",card);
+                    c=play_card(hand,card);
+                    if(c==-1)
+                        printf("You don't have that card");
+                }
 		    
 		    
 		    
@@ -113,7 +292,7 @@ void *player_kbd_handler(void *arg){
                 break;
 	    else 
 	    {
-	     printf("Supported commands:\nshow; play:RS; turn; exit");
+	     printf("Supported commands:\nhand; play:RS; turn; exit");
 	      
 	    }
             printf("\n");
@@ -134,6 +313,9 @@ void *player_gameplay_handler(void *arg){
     while (addr[0].turn == -1)
         pthread_cond_wait(&addr[0].cvar, &addr[0].mut);
     pthread_mutex_unlock(&addr[0].mut); 
+    
+    printf("bytesread %d\n",read(fd_read,hand,sizeof(hand)));
+    printf("\nrecebytes:%d\n",log_game("receive-cards",hand,ownNUMBER,ownNAME));
     //puts("\nOIIII");
   //  printf("\n[Dealer%c] %s is the one playing.\npromptPLAYER > ",addr[0].turn,addr[0].players[addr[0].turn].nickname);
     
@@ -175,8 +357,11 @@ int check=0,i;
 
 
 //        int distributing_cards(card *cards,int deck_size,char fifo[]){
-    for(i=0;i<addr[0].in;i++) 
+    for(i=0;i<addr[0].in;i++) {
         distributing_cards(addr[0].cards,&addr[0].deck_size,addr[0].players[i].FIFOname,i);
+        
+    }
+    printf("\ndisbytes:%d\n",log_game("deal", "-",0,ownNAME));
 
     print_shdata(addr[0]);
 
@@ -228,26 +413,30 @@ int distributing_cards(card *cards,int *deck_size,char *fifo,int player_number){
     char hand_str[150]="";
 
 printf("------>PLAYER%s",fifo);
+int check=0;
 for(j=0;j<HANDLIMIT;j++){
         hand[j]=cards[j]; 
         printf("[%s %c]",hand[j].rank,hand[j].suit);
     }
-    hand_str[0]='\0';
+    //hand_str[0]='\0';
     char suit[]={'c','d','h','s'};
     int size;
     for(i=0;i<4;i++){
         for(j=0;j<HANDLIMIT;j++){
             if(hand[j].suit==suit[i]){
+                check=1;
                 strcat(hand_str,hand[j].rank);
                 size=strlen(hand_str);
                 hand_str[size]=hand[j].suit;
-                hand_str[size+1]='\0';
+                //hand_str[size+1]='\0';
                 strcat(hand_str,"-");
             }
         }
-
+if(check){
         hand_str[strlen(hand_str)-1]='\0';
         strcat(hand_str,"/");
+        check=0;
+        }
     }
     hand_str[strlen(hand_str)-1]='\0';
     
@@ -374,7 +563,7 @@ int cleanall(shdata *addr, int shmid){
     }
     shmdt(addr);
     shmctl(shmid, IPC_RMID, NULL); 
-    close(log_fd);
+    
     return 0;
 }
 
@@ -414,11 +603,13 @@ void add_player_to_shdata(shdata *data,char* name){
 }
 void initalize_data(shdata *data, int room_size){
     
-    char buf[]="when	|who	|what	|result";
+    int log_fd;
 
+    char buf[]="when                | who                | what            | result\n";
     log_fd=open("shname.log", O_RDWR | O_CREAT, 0666);
-    write(log_fd,buf,sizeof(log_fd));
-    
+    write(log_fd,buf,sizeof(buf)-1);
+    close(log_fd);
+
     data[0].deck_size=52;
     data[0].nplayers=room_size;
     data[0].in=0;
